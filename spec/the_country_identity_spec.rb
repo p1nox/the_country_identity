7f32f1f30c72da11923a8851c3f6989e6bbb2372
p1nox <pinox101@gmail.com>
2014-07-19 00:36:59 -0400

    test requesting custom property from a remote server added

--- spec/the_country_identity_spec.rb
+++ spec/the_country_identity_spec.rb
@@ -2,9 +2,13 @@ require 'spec_helper'
 
 describe TheCountryIdentity do
 
-  describe 'No data for unknown country' do
-    it 'returns an empty object' do
+  describe 'Data fetched from remote RDF server' do
+    it 'returns a custom property' do
       country = subject::RDF.new("venezuela")
+      life_expectancy = country.get_property('lifeexpectancyatbirth_totalpopulation')
+
+      expect(life_expectancy).to be_instance_of(Float)
+      expect(life_expectancy).to eq 73.28
     end
   end
 
