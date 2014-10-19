31fcacde67857e7d87fdf24f73e5959aba0b33d5
p1nox <pinox101@gmail.com>
2014-10-19 19:21:42 -0300

    fetch method refactor to lazy return properties requesting for country information when needed

--- lib/the_country_identity/rdf.rb
+++ lib/the_country_identity/rdf.rb
@@ -6,46 +6,38 @@ module TheCountryIdentity
     @@URI_PREFIX       = 'http://wifo5-03.informatik.uni-mannheim.de/factbook/data/'
     @@STATEMENT_PREFIX = 'http://wifo5-04.informatik.uni-mannheim.de/factbook/ns#'
 
+    attr_accessor :cache
     attr_reader   :country_name
     attr_reader   :url
-    attr_reader   :repo
-    attr_accessor :data
+    attr_reader   :repository
 
-    def initialize(new_country_name)
-      fetch_country new_country_name
+    def initialize(country_name)
+      @cache = {}
+      @country_name = country_name.downcase.gsub(' ', '_').gsub('usa', 'united_states')
+      @url = @@URI_PREFIX + @country_name
     end
 
     def get_property(property_name)
-      return @data[property_name] if @data[property_name]
+      return @cache[property_name] if @cache[property_name]
 
-      unless @repo.nil?
-        options = { :predicate => ::RDF::URI("#{@@STATEMENT_PREFIX}#{property_name}") }
-        @repo.query(options) do |stmnt|
-          @data[property_name] = stmnt.object.value
+      begin
+        unless repo.nil?
+          options = { :predicate => ::RDF::URI("#{@@STATEMENT_PREFIX}#{property_name}") }
+          repo.query(options) do |stmnt|
+            @cache[property_name] = stmnt.object.value
+          end
         end
+      rescue => e
+        puts "Not able to get country information, through exception: #{e}"
       end
 
-      @data[property_name]
-    end
-
-    def fetch_country(country_name)
-      @data = {}
-      unless country_name.nil?
-        @country_name = country_name.downcase.gsub(' ', '_').gsub('usa', 'united_states')
-        @url = @@URI_PREFIX + @country_name
-
-        begin
-          fetch_rdf
-        rescue => e
-          puts "Not able to get country information, through exception: #{e}"
-        end
-      end
+      @cache[property_name]
     end
 
     private
 
-      def fetch_rdf
-        @repo = ::RDF::Repository.load(@url)
+      def repo
+        @repository ||= ::RDF::Repository.load(@url)
       end
 
   end
